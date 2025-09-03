9c80be9ea272aeb4b4216d05a57fc2cb9a7a01ab
Ivan <ivanmg10@outlook.es>
2025-09-03 15:58:13 +0200

    feat: create page and start create the about me component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import DefaultHeader from "./components/app-header/DefaultHeader";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 main-page-body">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="typewriter">
           <h1 className="text-5xl">Ivan Marquez Garcia.</h1>
--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -1,8 +1,44 @@
+import Image from "next/image";
+import picture from "../../../../public/yo.jpg";
+import "./about.css";
+
 export default function AboutPage() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
-      <h1 className="text-5xl">About Us</h1>
-      <p>Welcome to our about page!</p>
+    <div className="flex justify-center">
+      <div className="grid grid-flow-col grid-rows-3 gap-4 p-8 main-body w-2/3 about-me">
+        <div className="row-span-3 col-span-1 p-4 flex justify-around">
+          <div>
+            <div className=" max-w-4xl">
+              <h1 className="text-4xl font-bold mb-4">Sobre mi</h1>
+              <p className="mb-4">
+                Buenas! Mi nombre es Ivan, soy un desarrollador de aplicaciones
+                web con una experiencia laboral de 4 años, actualmente dedicado
+                al sector de la banca digital y abierto siempre a nuevos retos.
+              </p>
+              <p className="mb-4">
+                Estudie la formacion profesional en desarrollo de aplicaciones
+                web, donde adquiri habilidades en tecnologias basicas como HTML,
+                CSS y JS para mas adelante, de manera autodidacta y dentro de mi
+                entorno profesional, React entre otros frameworks y librerias,
+                asi como Node.js para el desarrollo del lado del servidor.
+              </p>
+              <p className="mb-4">
+                Me considero una navaja suiza, ya que fuera de mis habilidades
+                como programador tambien tengo experiencia en tener trato
+                directo con clientes, formar a otros desarrolladores y aprender
+                nuevas tecnologias si es necesario.
+              </p>
+            </div>
+          </div>
+          <div>
+            <Image
+              className="h-48 w-full object-cover md:h-full md:w-90 mask-radial-[100%_100%] mask-radial-from-75% mask-radial-at-right rounded-r-lg"
+              src={picture}
+              alt="Ivan"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
