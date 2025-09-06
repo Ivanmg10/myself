76ef05ac46a43462a7f806dc0fce07f64e066ed5
Ivan <ivanmg10@outlook.es>
2025-09-06 21:33:51 +0200

    Feat: allow the slug page to be deployed

--- src/app/pages/projects/[slug]/page.tsx
+++ src/app/pages/projects/[slug]/page.tsx
@@ -1,4 +1,16 @@
-export default function AboutPage({ params }: { params: { slug: string } }) {
+interface AboutPageProps {
+  params: { slug: string };
+}
+
+const projects = ["project-1", "project-2", "project-3"];
+
+export async function generateStaticParams(): Promise<
+  AboutPageProps["params"][]
+> {
+  return projects.map((slug) => ({ slug }));
+}
+
+export default function AboutPage({ params }: AboutPageProps) {
   return (
     <div className="font-sans grid grid-rows-[auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
       <h1 className="text-5xl">My Projects</h1>
