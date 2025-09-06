cf3050f36f0fa1f99aa913392367ff7c32e47782
Ivan <ivanmg10@outlook.es>
2025-09-06 20:49:57 +0200

    feat: improve navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#a29bfe] text-background gap-2 hover:bg-[#6c5ce7] dark:hover:bg-[#6c5ce7] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/pages/projects"
           >
             Mis proyectos
@@ -50,7 +50,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="/github.svg"
             alt="Window icon"
             width={16}
             height={16}
--- src/app/pages/projects/page.tsx
+++ src/app/pages/projects/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import "./projects.css";
-import flowey from "../../../../public/flowey.jpg";
+import flowey from "../../../../public/images/flowey.jpg";
 import Image from "next/image";
 
 export default function AboutPage() {
