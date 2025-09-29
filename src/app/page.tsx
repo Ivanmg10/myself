9a2df6bca3a20d48e41690c1ca9b8df3916cdd0d
Ivan <ivanmg10@outlook.es>
2025-09-29 16:37:11 +0200

    feat: fix button redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row w-full">
           <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#a29bfe] text-background gap-2 hover:bg-[#6c5ce7] dark:hover:bg-[#6c5ce7] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[160px]"
-            href="/pages/projects text-black"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#a29bfe] text-background gap-2 hover:bg-[#6c5ce7] dark:hover:bg-[#6c5ce7] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[160px] text-black"
+            href="/pages/projects"
           >
             Mis proyectos
           </Link>
